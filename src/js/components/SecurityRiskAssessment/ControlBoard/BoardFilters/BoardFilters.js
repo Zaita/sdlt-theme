4791e1618ff5f388e8bcc849b974c7ad1ab3da5b
al-abbas-nz <al.mus930@gmail.com>
2022-02-14 15:34:02 +1300

    MINOR: formatting

--- src/js/components/SecurityRiskAssessment/ControlBoard/BoardFilters/BoardFilters.js
+++ src/js/components/SecurityRiskAssessment/ControlBoard/BoardFilters/BoardFilters.js
@@ -9,10 +9,10 @@ export default class BoardFilters extends Component {
   render() {
     return (
       <div className='board-filters'>
-          <BoardSearch label={'Key words'} />
-          <BoardSelect label={'Risk category'} filterParameters={riskCategories} />
-          <BoardSelect label={'Sort by'} filterParameters={sortBy} addClass={'sort-by'} />
-          <BoardCheckbox label={'Show not applicable'} />
+        <BoardSearch label={'Key words'} />
+        <BoardSelect label={'Risk category'} filterParameters={riskCategories} />
+        <BoardSelect label={'Sort by'} filterParameters={sortBy} addClass={'sort-by'} />
+        <BoardCheckbox label={'Show not applicable'} />
       </div>
     )
   }
