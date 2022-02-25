8b1bd9c4c9a8478f7bc85b1931f78161bbfea1da
Jessica Moorthy <jessicamoorthy@catalyst.net.nz>
2022-02-25 15:06:13 +1300

    NEW: 90587 add risk rating help modal

---
+++ src/js/components/SecurityRiskAssessment/RiskAssessmentHelpModalContainer.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import CloseIcon from "../../../img/icons/close.svg";
+import helpIcon from "../../../img/icons/help-outline.svg";
+import RiskRatingThresholdContainer from "./RiskRatingThresholdContainer";
+import ReactModal from "react-modal";
+
+class RiskAssessmentHelpModalContainer extends Component {
+  state = {
+    showModal: false,
+  };
+
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  renderCurrentRiskRatingContent() {
+    return (
+      <div className="content">
+        <div
+          className="modal-help-text"
+          dangerouslySetInnerHTML={{
+            __html: this.props.helpText
+          }}
+        />
+        <RiskRatingThresholdContainer
+          riskRatingThresholds={this.props.riskRatingThresholds}
+        />
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div className="HelpModalContainer">
+        <img src={helpIcon} onClick={() => this.handleOpenModal()}/>
+        <ReactModal
+          isOpen={this.state.showModal}
+          ariaHideApp={false}
+          parentSelector={() => {
+            return document.querySelector(".SecurityRiskAssessmentContainer");
+          }}
+        >
+          <div className="modal-header">
+            <span className="header-title">
+              <h1>{this.props.title}</h1>
+            </span>
+            <div className="close-icon-container" onClick={this.handleCloseModal}>
+              <img src={CloseIcon} />
+            </div>
+          </div>
+          {this.props.title === "Current Risk Rating" && this.renderCurrentRiskRatingContent()}
+        </ReactModal>
+      </div>
+    );
+  }
+}
+export default RiskAssessmentHelpModalContainer;
