82dbedaeac2a9b9938255ce1f1b0d1cfa039f578
jessicamoorthy <jessicamoorthy@catalyst.net.nz>
2022-05-10 15:54:01 +1200

    FIX: 90520 enable filters after dragging when the position of card stays same

--- dist/js/common.bundle.js
+++ dist/js/common.bundle.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "daae165530fc10041427";
+/******/ 	var hotCurrentHash = "2fe16912281e4c424469";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
