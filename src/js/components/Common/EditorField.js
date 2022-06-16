acb526167348e2ce0c7af0693550561566ea85a8
Roopam <roopamjain@catalyst.net.nz>
2022-06-17 11:31:12 +1200

    NEW: RM91529 save button logic and tidy up

---
+++ src/js/components/Common/EditorField.js
@@ -0,0 +1,48 @@
+// @flow
+import React, { useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+
+type Props = {
+  heading: string,
+  helpText: string,
+  initialValue: string,
+  fieldName: string,
+  onBlurUpdate: function,
+};
+
+export default function EditorField(props: Props) {
+  const { heading, helpText, initialValue, fieldName, onBlurUpdate } = props;
+  const [isTextAreaFocus, setIsTextAreaFocus] = useState(false);
+
+  const handleOnBlur = (event) => {
+    setIsTextAreaFocus(false);
+    onBlurUpdate(fieldName, event.target.getContent());
+  };
+
+  return (
+    <div className="editor-container">
+      <h5>{heading}</h5>
+      <p className="help-text">{helpText}</p>
+      <div className={`editor-text-field ${isTextAreaFocus ? "focus" : ""}`}>
+        <Editor
+          initialValue={initialValue}
+          textareaName={fieldName}
+          init={{
+            selector: "textarea",
+            height: "73",
+            menubar: false,
+            toolbar: false,
+            statusbar: false,
+            content_style:
+              "body { font-size: 11px; line-height: 16px; }" +
+              "html { scrollbar-color: #2371A6 #fff; }",
+            skin_url:
+              "resources/vendor/silverstripe/admin/thirdparty/tinymce/skins/silverstripe",
+          }}
+          onFocus={() => setIsTextAreaFocus(true)}
+          onBlur={(event) => handleOnBlur(event)}
+        />
+      </div>
+    </div>
+  );
+}
